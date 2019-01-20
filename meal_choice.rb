14276754b067e74f67074c76b82e7476f701302b
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-01-20 23:51:54 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,5 +1,5 @@
-def meal_choice(meal_choice: meat)
-  puts meal
+def meal_choice(meal_choice = "meat")
+  puts meal_choice
 end
 
 # Define a method meal_choice that returns the meal_choice that was
